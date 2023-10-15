51b20265c20c92039b1c38d0d803a10dcf755ba0
ruggero61 <ruggerosoftwareengineer@gmail.com>
2023-10-15 10:01:49 +0200

    Added MyAction class and delegate

--- Program.cs
+++ Program.cs
@@ -4,5 +4,20 @@ class Program
 {
   static void Main(string[] args)
   {
+    MyAction testAction = () => Console.WriteLine("Hello Test Action...");
+    testAction += () => Console.WriteLine("Hello Test Action 2...");
+    testAction.Invoke();
+    MyTask.Run(() => Console.WriteLine("Hello Task..."));
+  }
+}
+
+public delegate void MyAction();
+
+public static class MyTask
+{
+  public static void Run(MyAction action)
+  {
+    Thread thread = new Thread(() => action());
+    thread.Start();
   }
 }
